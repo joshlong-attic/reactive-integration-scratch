2b9b44fdd2c5add2c7472eeb56e464470f85143b
Josh Long <starbuxman@gmail.com>
2018-02-11 07:13:39 +0100

    reactive Spring Integration messaging gateway

--- reactive-integration/src/main/java/com/example/reactiveintegration/ReactiveIntegrationApplication.java
+++ reactive-integration/src/main/java/com/example/reactiveintegration/ReactiveIntegrationApplication.java
@@ -1,55 +1,39 @@
 package com.example.reactiveintegration;
 
 import lombok.extern.java.Log;
-import org.reactivestreams.Publisher;
 import org.springframework.amqp.core.*;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.amqp.dsl.Amqp;
+import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.channel.MessageChannels;
-import org.springframework.integration.handler.GenericHandler;
 import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.support.MessageBuilder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
-
-import java.util.Map;
 
 @Log
 @SpringBootApplication
 public class ReactiveIntegrationApplication {
 
+	public static final String REQUESTS = "requests";
+
 	@Configuration
-	@RestController
+	@IntegrationComponentScan
 	public static class AmqpGatewayConfiguration {
 
-		private String tips = "tips";
+		private final String tips = "tips";
 
-		// https://docs.spring.io/spring-integration/reference/html/messaging-endpoints-chapter.html#_reactor_mono
 		// REACTIVE GATEWAYS
+		// https://docs.spring.io/spring-integration/reference/html/messaging-endpoints-chapter.html#_reactor_mono
 
-		@PostMapping("/send")
-		Publisher<Boolean> send(@RequestBody Mono<Map<String, String>> incoming) {
-			return
-					incoming
-							.map(map -> map.getOrDefault("message", "world"))
-							.map(msg -> MessageBuilder.withPayload(msg).build())
-							.map(msg -> requests().send(msg));
-		}
-
-		@Bean
+		@Bean(REQUESTS)
 		MessageChannel requests() {
-			return MessageChannels.direct().get();
-		}
-
-		@Bean
-		MessageChannel responses() {
 			return MessageChannels.direct().get();
 		}
 
@@ -74,19 +58,26 @@ public class ReactiveIntegrationApplication {
 					IntegrationFlows
 							.from(requests())
 							.handle(Amqp.outboundGateway(template).routingKey(this.tips))
-							.handle((GenericHandler<String>) (payload, headers) -> {
-								log.info("received response: " + payload);
-								headers.forEach((k, v) -> log.info("\t" + k + '=' + v));
-								return null;
-							})
 							.get();
 		}
-	}
 
+		@Bean
+		ApplicationRunner run(UppercaseGateway gateway) {
+			return args ->
+					gateway
+							.uppercase("world")
+							.subscribe(x -> log.info("received " + x));
+		}
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveIntegrationApplication.class, args);
 	}
 }
 
+@MessagingGateway
+interface UppercaseGateway {
 
+	@Gateway(requestChannel = ReactiveIntegrationApplication.REQUESTS)
+	Mono<String> uppercase(String text);
+}
